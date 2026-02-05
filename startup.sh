ee1fed7b0eca040e189024cdb6aab4f2be9bd2fb
Chris <chrishawkins@live.co.uk>
2026-02-05 17:03:32 +0000

    fixed typos

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Go to project directory
-cd /home/pi/netportscan
+cd /home/pi/repos/netportscan
 
 # Start the lldpd scanning script in the background
-./home/pi/netportscan/scan_lldpd.sh &
+./home/pi/repos/netportscan/scan_lldpd.sh &
 
 # Activate the python venv
-source /home/pi/netportscan/venv/bin/activate
+source /home/pi/netportscan/bin/activate
 
 # Run Python script
 python tui.py
